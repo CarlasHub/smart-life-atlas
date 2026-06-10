42bdaf7a9dcc93c0b0244381cd1e177a8bc1d072
CarlasHub <34892979+CarlasHub@users.noreply.github.com>
2026-06-10 14:55:37 +0100

    Support Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'node:process'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/smart-life-atlas/',
+  base: process.env.VERCEL ? '/' : '/smart-life-atlas/',
   plugins: [react()],
 })
